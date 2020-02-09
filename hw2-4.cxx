d5cd7e885f522116d26cea0f92192e28435277b4
Griffin Kowash <gkowash@gmail.com>
2020-02-08 23:58:07 -0800

    Completed version of Problem 4.

--- hw2-4.cxx
+++ hw2-4.cxx
@@ -1,17 +1,83 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main() {
-  int num = 9009;
-  string numStr = to_string(num);
 
-  cout << "Number string: " << numStr << endl;
+/*Program to find the largest palindromic number that is a product of
+  two three-digit numbers. */
+
+
+
+//list all discovered palindromes in the console
+void printPalindromes(vector<int> vec) {
+  cout << "Printing all palindromes.\n\n";
+  for (int i = 0; i < vec.size(); i++) {
+    cout << vec[i] << endl;
+  };
+  cout << endl;
+}
+
+
+//reverse the order of characters in a string
+string reverseString(string str) {
+  string strRev;
+  for (int i = str.size() - 1; i >= 0; i--) {
+    strRev.append(1, str[i]);
+  }
+  return strRev;
+}
+
+
+//compare number with its reversed form to determine whether it's a palindrome
+bool isPalindrome(int num) {
+  string numString = to_string(num);
+  return numString == reverseString(numString);
+}
+
+
+
+int main(int argc, char **argv) {  
+  vector<int> palindromes;
+  vector<vector<int>> abValues;
+  vector<int> ab;
+  int product;
+  bool show = false;
+
+  //use option "show" to print all palindromes found to the console
+  if (argc > 1) {
+    if (string(argv[1]) == "show") {
+      show = true;
+    }
+  }
+
+  //loop through all combinations of three-digit numbers and record palindromes
+  for (int a = 999; a > 99; a--) {
+    for (int b = a; b > 99; b--) {
+      product = a*b;
+      if (isPalindrome(product)) {
+	palindromes.push_back(product);
+	abValues.push_back({a, b});
+      }
+    }
+  }
+
+  //set 'showAll' to true to see all palindromes discovered
+  if (show) {
+    printPalindromes(palindromes);
+  }
+
+  //find the largest palindrome and the pair of numbers that produced it
+  vector<int>::iterator maxPalindrome = max_element(palindromes.begin(), palindromes.end());
+  ab = abValues[distance(palindromes.begin(), maxPalindrome)];
+  int a = ab[0];
+  int b = ab[1];
+
+  //print results to the console
+  cout << "Total palindromes: " << palindromes.size() << endl;
+  cout << "Largest palindrome: " << *maxPalindrome << " = " << a << "*" << b << endl;
   
-  string numStrRev = reverse(numStr.begin(), numStr.end());
-  bool ans = (numStr == numStrRev);
-
-  cout << "Is " << numStr << " a palindrome? " << ans;
+  return 1;
 }
